97fabed867c50b6c5eb5be83004436fe5c3ebfe4
Marc Koster <marckoster@gmail.com>
2016-01-21 22:57:59 +0100

    update login subscribe

--- client/lib/startup.js
+++ client/lib/startup.js
@@ -11,33 +11,43 @@ if (Meteor.isCordova) {
             }
 
 
-//TODO asynchrone scheisse funzt so net ganz ........
-           // Meteor.subscribe('location');
-            //var currentLocation = Location.findOne({locationname: "shisha"});
 
-            var networkState = navigator.connection.type;
-            if (networkState == "none") {
-                Session.set("wlanConnected", false)
-                IonPopup.alert({
-                    title: 'Keine Wlan Verbindung',
-                    template: 'Es muss sich mit dem Lokal Wlan verbunden werden um die Anwendung zu nutzen'
-                });
-            }
-            if (networkState == "wifi") {
-                WifiWizard.getCurrentSSID(function (success) {
-                    if (success.indexOf("TP-LINK_84D190") > 0) {
-                        Session.set("wlanConnected", true)
-                    } else {
+
+
+            Meteor.subscribe("location", {
+                onReady: function () {
+                    
+                    //TODO asynchrone scheisse funzt so net ganz ........
+                    Meteor.subscribe('location');
+                    var currentLocation = Location.findOne({locationname: "shisha"});
+                    var networkState = navigator.connection.type;
+                    if (networkState == "none") {
                         Session.set("wlanConnected", false)
                         IonPopup.alert({
-                            title: 'Falsches Wlan',
+                            title: 'Keine Wlan Verbindung',
                             template: 'Es muss sich mit dem Lokal Wlan verbunden werden um die Anwendung zu nutzen'
                         });
                     }
-                }, function (error) {
-                    console.log(error)
-                });
-            }
+                    if (networkState == "wifi") {
+                        WifiWizard.getCurrentSSID(function (success) {
+                            if (success.indexOf(currentLocation.wlanssid) > 0) {
+                                Session.set("wlanConnected", true)
+                            } else {
+                                Session.set("wlanConnected", false)
+                                IonPopup.alert({
+                                    title: 'Falsches Wlan',
+                                    template: 'Es muss sich mit dem Lokal Wlan verbunden werden um die Anwendung zu nutzen'
+                                });
+                            }
+                        }, function (error) {
+                            console.log(error)
+                        });
+                    }
+
+                },
+                onError: function () { console.log("onError", arguments); }
+            });
+
 
         }
     );
